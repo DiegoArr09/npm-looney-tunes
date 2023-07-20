0937b79eefd9bd7ca6f03c68fe1e8c4d4d861ebc
DiegoArr09 <diegoarrigoni12@gmail.com>
2023-07-20 11:04:41 -0300

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const looneys = require("./looneys.js");
+
+function all() {
+  return looneys;
+}
+function random() {
+  for (let i = 0; i < looneys.length; i++) {
+    const position = Math.floor(Math.random() * looneys.length);
+    const looneyRandom = looneys[position];
+    return looneyRandom;
+  }
+}
+function getLooney(looneyName) {
+  for (let looney of looneys) {
+    if (looney.name === looneyName) {
+      return looney;
+    }
+  }
+}
+function friends(looneyName) {
+  for (let looney of looneys) {
+    if (looney.name === looneyName) {
+      return looney.friends;
+    }
+  }
+}
+function enemies(looneyName) {
+  for (let looney of looneys) {
+    if (looney.name === looneyName) {
+      return looney.enemies;
+    }
+  }
+}
+
+module.exports = {
+  all,
+  random,
+  getLooney,
+  friends,
+  enemies,
+};
